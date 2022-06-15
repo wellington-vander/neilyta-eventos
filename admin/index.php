450c83f5871922a678ab4827024b8ccbed4b1e0d
wellington-vander <107505080+wellington-vander@users.noreply.github.com>
2022-06-15 08:32:51 -0300

    Navbar

--- admin/index.php
+++ admin/index.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Painel do Admistrador</title>
 </head>
 <body>
     <nav>
-        <div class="nav-wrapper">
-            
+        <div class="nav-wrapper bg-color">
+            <a href="#!" class="brand-logo"><img id="nav-logo" src="img/neilyta.svg" alt="" title="Neilyta Eventos"></a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="">Home</a></li>
+                <li><a class="btn-small bg-color" href="">Sair</a></li>
+            </ul>
         </div>
     </nav>
 
